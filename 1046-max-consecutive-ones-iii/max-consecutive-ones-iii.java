19bff385285b5162bd9bc07a7d49a90f194e6b77
J DAMODARA PRASAD REDDY <234g1a0534@srit.ac.in>
2026-06-04 11:41:37 +0530

    Time: 4 ms (47.65%) | Memory: 52.3 MB (17.54%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -3,10 +3,9 @@ class Solution {
         int c=0;
         int l=0,max=0,mf=0;
         for(int r=0;r<nums.length;r++){
-            if(nums[r]==1)c++;
-            mf=Math.max(mf,c);
-            if(r-l+1-mf>k){
-                if(nums[l++]==1)c--;
+            if(nums[r]==0)c++;
+            if(c>k){
+                if(nums[l++]==0)c--;
             }
             max=Math.max(max,r-l+1);
         } return max;                                       
