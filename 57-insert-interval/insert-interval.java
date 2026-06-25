cdae8251b7f01c308f28823363d700ae0cc7dc4b
J DAMODARA PRASAD REDDY <234g1a0534@srit.ac.in>
2026-06-25 15:21:10 +0530

    Time: 1 ms (98.01%) | Memory: 47.3 MB (18.85%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,57 +1,24 @@
 class Solution {
-    public int[][] insert(int[][] arr, int[] newInt) {
-        // if(nums.length==0){
-        //     return new int[][]{newInt};
-        // }
-        // List<int[]> list= new ArrayList<>();
-        // int e1=nums[0][0];
-        // int e2=nums[0][1];
-        // int s=1,fl=0;
-        // while(s<nums.length){
-        //     if(nums[s][0]<=e2){
-        //         e2=Math.max(e2,nums[s][1]);
-        //         s++;
-        //     }else if(newInt[0]<=e2 && fl==0){
-        //         e1=Math.min(e1,newInt[0]);
-        //         e2=Math.max(e2,newInt[1]);
-        //         fl=1;
-        //     }else{
-        //      list.add(new int[]{e1,e2});
-        //      e1=nums[s][0];
-        //      e2=nums[s][1];
-        //      s++;
-        //   }
-        // }
-        // list.add(new int[]{e1,e2});
-
-        // return list.toArray(new int[0][]);
-
-         ArrayList<int[]> list=new ArrayList<>();
-         int[][] nums=new int[arr.length+1][2];
-         for(int i=0;i<arr.length;i++){
-                nums[i][0]=arr[i][0];
-                nums[i][1]=arr[i][1];
-         }
-         nums[arr.length][0]=newInt[0];
-         nums[arr.length][1]=newInt[1];
-        Arrays.sort(nums,(a,b)->a[0]-b[0]);
-        int s=1,e=0;
-        int el1=nums[0][0];
-        int el2=nums[0][1];
-        while(s<nums.length){
-           if(nums[s][0]<=el2){
-            el2=Math.max(el2,nums[s][1]);
-           }else{
-            list.add(new int[]{el1,el2});
-            el1=nums[s][0];
-            el2=nums[s][1];
-           }
-              e++;
-              s++;
+    public int[][] insert(int[][] nums, int[] newInt) {
+        if(nums.length==0){
+            return new int[][]{newInt};
         }
-        list.add(new int[]{el1,el2});
-
-        int[][] ans = list.toArray(new int[0][]);
-        return ans;
+        List<int[]> list= new ArrayList<>();
+        int s=0;
+        while(s<nums.length && newInt[0]>nums[s][1]){
+            list.add(new int[]{nums[s][0],nums[s][1]});
+            s++;
+        }
+        while(s<nums.length && nums[s][0]<=newInt[1] ){
+               newInt[0]=Math.min(nums[s][0],newInt[0]);
+               newInt[1]=Math.max(nums[s][1],newInt[1]);
+               s++;
+        }
+        list.add(newInt);
+        while(s<nums.length){
+            list.add(new int[]{nums[s][0],nums[s][1]});
+            s++;
+        }
+       return list.toArray(new int[0][]);
     }
 }
