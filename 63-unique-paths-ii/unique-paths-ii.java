0ff4976ec8556e6218cf00cacd4ca31e58527c22
J DAMODARA PRASAD REDDY <234g1a0534@srit.ac.in>
2026-07-06 13:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (63.91%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -2,35 +2,24 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
         int n=obstacleGrid[0].length;
-
-        if( obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1)return 0;
-        
-        for(int i=0;i<m;i++){
-              for(int j=0;j<n;j++){
-                if( obstacleGrid[i][j]==1) obstacleGrid[i][j]=-1;
-            }
+         if( obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1)return 0;
+         
+       int dp[][]= new int[m][n];
+        for(int i=m-1;i>=0;i--){
+            if(obstacleGrid[i][n-1]==1) break;
+            dp[i][n-1]=1;
+        }
+        for(int j=n-1;j>=0;j--){
+            if(obstacleGrid[m-1][j]==1) break;
+            dp[m-1][j]=1;
         }
 
-        int ans= paths(0,0,m,n,obstacleGrid);
-        return ans;
-    }
-    
-     static int paths(int i,int j,int m,int n,int[][] dp){
-          
-          if(i==m-1 && j==n-1){
-              return 1;
-          }
-          if(dp[i][j]!=0 && dp[i][j]!=-1){
-            return dp[i][j];
-          }
-          int down=0,right=0;
-          if( i+1<m && dp[i+1][j]!=-1){
-             down= paths(i+1,j,m,n,dp);
-          }
-           if(j+1<n && dp[i][j+1]!=-1 ){
-             right= paths(i,j+1,m,n,dp);
-          }
-          
-         return dp[i][j]=down + right;
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                if(obstacleGrid[i][j]!=1)
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 }
