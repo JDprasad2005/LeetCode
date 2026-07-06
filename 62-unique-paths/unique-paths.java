90111a4ce137bf994a59df0834984aaf018edb64
J DAMODARA PRASAD REDDY <234g1a0534@srit.ac.in>
2026-07-06 12:27:00 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (96.12%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,29 +1,17 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+       int dp[][]= new int[m][n];
+        for(int i=m-1;i>=0;i--){
+            dp[i][n-1]=1;
         }
-
-        int ans= paths(0,0,m,n,dp);
-        return ans;
-    }
-    
-     static int paths(int i,int j,int m,int n,int[][] dp){
-          if(i==m-1 && j==n-1){
-              return 1;
-          }
-          if(dp[i][j]!=-1){
-            return dp[i][j];
-          }
-          int down=0,right=0;
-          if(i+1<m){
-             down= paths(i+1,j,m,n,dp);
-          }
-           if(j+1<n){
-             right= paths(i,j+1,m,n,dp);
-          }
-          
-         return dp[i][j]=down + right;
+        for(int j=n-1;j>=0;j--){
+            dp[m-1][j]=1;
+        }
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 }
