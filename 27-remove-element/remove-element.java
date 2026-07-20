cd2e022b84b1ff216e59cacf7ba893292f04fd28
J DAMODARA PRASAD REDDY <234g1a0534@srit.ac.in>
2026-07-21 00:26:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (62.06%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,10 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-          int i=0;
-         for(int j=0;j<nums.length;j++){
-          if(nums[j]!=val){
-            nums[i++]=nums[j];
-           }
-        } return i;
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            if(nums[left]==val){
+                nums[left]=nums[right--];
+            }else{
+                left++;
+            }
+        } return right+1;
     }
 }
