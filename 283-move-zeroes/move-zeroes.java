cefe47f47fae7d2e646fbfc7ad5c8c9b3b39f082
J DAMODARA PRASAD REDDY <234g1a0534@srit.ac.in>
2026-07-29 01:23:07 +0530

    Time: 3 ms (18.59%) | Memory: 48 MB (20.41%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        for(i=0;i<nums.length;i++){
-            if(nums[i]==0) break;
-        }
+        int i = 0;
 
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[j]!=0){
-                swap(nums,i++,j);
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
             }
         }
     }
-
-    static void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    } 
 }
