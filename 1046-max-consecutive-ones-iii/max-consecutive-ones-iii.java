0eb1a8ae7bd202bdc30112972af122e6211652a6
J DAMODARA PRASAD REDDY <234g1a0534@srit.ac.in>
2026-07-29 14:16:34 +0530

    Time: 3 ms (95.99%) | Memory: 52 MB (89.31%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,13 +1,22 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
-        int c=0;
-        int l=0,max=0,mf=0;
-        for(int r=0;r<nums.length;r++){
-            if(nums[r]==0)c++;
-            if(c>k){
-                if(nums[l++]==0)c--;
+        int n = nums.length;
+        int l= 0;
+        int zeroes = 0;
+        int maxLength = 0;
+        for(int r = 0; r<n; r++){
+            if(nums[r] == 0){
+                zeroes++;
             }
-            max=Math.max(max,r-l+1);
-        } return max;                                       
+            while(zeroes > k){
+               if(nums[l] == 0){
+                zeroes--;
+               } 
+               l++;
+            }
+            int len = r-l+1;
+            maxLength = Math.max(len, maxLength);
+        }
+        return maxLength;
     }
 }
