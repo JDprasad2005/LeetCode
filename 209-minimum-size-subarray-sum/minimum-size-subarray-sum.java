bbdc36bae390de9a2ab35531ab1b76bf73c1626f
J DAMODARA PRASAD REDDY <234g1a0534@srit.ac.in>
2026-08-01 02:15:31 +0530

    Time: 1 ms (99.92%) | Memory: 68.9 MB (70.79%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,18 +1,16 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int  n = nums.length;
-        int l = 0;
-        int minLength = Integer.MAX_VALUE;
-        int sum = 0;
-        for(int r = 0; r<n; r++){
-             sum = sum + nums[r];
-             while(sum >= target){
-               minLength = Math.min(r-l+1, minLength);
-               
-                    sum = sum - nums[l];
-                    l++;
-             }
-        }
-        return minLength == Integer.MAX_VALUE ?0 : minLength;
+       int i=0,j=0,min=nums.length+1;
+       long sum=0;
+
+       while(j<nums.length){
+          sum+=nums[j];
+
+          while(sum>=target){
+            min=Math.min(min,j-i+1);
+            sum-=nums[i++];
+          }
+          j++;
+       } return (min==nums.length+1)?0:min;
     }
 }
